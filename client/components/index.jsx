586d1c8f795c55993c4816f3e0af82add4e9a6b6
Kestrad <firedreamer@gmail.com>
2018-11-05 16:00:31 -0800

    Tweaked files to make combining in proxy servers easier

--- client/components/index.jsx
+++ client/components/index.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import '../../public/styles.css';
 
-ReactDOM.render(<App />, document.getElementById('more'));
+// ReactDOM.render(<App />, document.getElementById('more'));
+
+window.Similar = App;
