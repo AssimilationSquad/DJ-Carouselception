1eccb285449d9463d04db4b788b771bb9e342ae1
Kestrad <firedreamer@gmail.com>
2018-11-06 15:06:06 -0800

    Changed seed script a little to run without numpy installed
    Since my repo includes an already generated set of data, got rid of
    line in seeding script that runs python script to generate random
    data.

--- db/seed.js
+++ db/seed.js
@@ -5,7 +5,7 @@ const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
 
 async function insertListings() {
-  await execFile('python',[__dirname + "/randomData.py"]);
+  // await execFile('python',[__dirname + "/randomData.py"]);
   var contents = fs.readFileSync(__dirname + '/seed.txt', 'utf8');
   contents = JSON.parse(contents);
   console.log('json stuff done');
